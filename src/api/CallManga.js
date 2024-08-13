f4449c0845585007d8c7548506b994993baccd4b
AlessandroAismondo <alessandro.aismonodo@gmail.com>
2024-08-13 22:08:56 +0200

    Update 13/08 API Calls

--- src/api/CallManga.js
+++ src/api/CallManga.js
@@ -1,12 +1,12 @@
 export async function catchAnime(id) {
-    const url = `https://api.jikan.moe/v4/anime?q=naruto`;
+    const url = `https://myanimelist.p.rapidapi.com/manga/${id}`;
     const options = {
         method: 'GET',
         mode: 'cors',
-        // headers: {
-        //     'x-rapidapi-key': 'b988c5c6b4msh4bb42f26f293dbfp1764e8jsnb702cbc62581',
-        //     'x-rapidapi-host': 'myanimelist.p.rapidapi.com'
-        // }
+        headers: {
+            'x-rapidapi-key': 'b988c5c6b4msh4bb42f26f293dbfp1764e8jsnb702cbc62581',
+            'x-rapidapi-host': 'myanimelist.p.rapidapi.com'
+        }
     };
 
     try {
